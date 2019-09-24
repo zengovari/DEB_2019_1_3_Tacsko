0adb37fe97cd947aecf033ba0dec8b47afa22fe7
Zengővári Balázs <zengovaribalazs98@gmail.com>
2019-09-24 18:36:27 +0200

    Handler.java Map készítve <Ország, Városok> formában

---
+++ TacskoWeather/src/main/java/Model/City.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class City {
+
+    private int id;
+    private String name;
+    private String country;
+    private Coord coord;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+
+}
